3f5b4b2f18dede1f78d8ee03e1b860ab963c0caf
Emol Li <duan.li@mail.mcgill.ca>
2017-09-23 10:56:26 -0400

    init

---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour {
+
+    public GameObject wall;
+    public float wallLength = 1.0f;
+    public int xSize = 5;   // 5 rows in x axis
+    public int ySize = 5;   // 5 rows in y axis
+    private Vector3 initialPos;
+    private GameObject wallHolder;
+
+	// Use this for initialization
+	void Start () {
+        CreateWalls();
+	}
+	
+    void CreateWalls()
+    {
+        wallHolder = new GameObject();
+        wallHolder.name = "Maze";
+
+        initialPos = new Vector3((-xSize / 2) + wallLength / 2, 0.0f, (-ySize / 2
+            ) + wallLength / 2);
+        Vector3 myPos = initialPos;
+        GameObject tempWall;
+
+        // For x Axis
+        for (int i = 0; i < ySize; i++)
+        {
+            for (int j = 0; j <= xSize; j++)
+            {
+                myPos = new Vector3(initialPos.x + (j * wallLength) - wallLength / 2, 0.0f, initialPos.z + (i * wallLength) - wallLength / 2);
+                tempWall = Instantiate(wall, myPos, Quaternion.identity) as GameObject;
+                tempWall.transform.parent = wallHolder.transform;
+            }
+        }
+
+        // For y Axis
+        for (int i = 0; i <= ySize; i++)
+        {
+            for (int j = 0; j < xSize; j++)
+            {
+                myPos = new Vector3(initialPos.x + (j * wallLength), 0.0f, initialPos.z + (i * wallLength) - wallLength);
+                tempWall = Instantiate(wall, myPos, Quaternion.Euler(0.0f, 90.0f, 0.0f)) as GameObject;
+                tempWall.transform.parent = wallHolder.transform;
+            }
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
