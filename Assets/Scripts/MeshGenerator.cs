81d1c2a43112d665322ea86bd24917042c5242ff
Emol Li <duan.li@mail.mcgill.ca>
2017-09-24 23:26:57 -0400

    surface

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -8,12 +8,113 @@ public class MeshGenerator : MonoBehaviour {
     int[] mTris;    
 	// Use this for initialization
 	void Start () {
+
+        //Create new mesh - this will hold our triangle
+        Mesh newMesh = new Mesh();
+
+        //This makes the mesh available to other components
+        gameObject.AddComponent<MeshFilter>();
+
+        //This makes it visible
+        gameObject.AddComponent<MeshRenderer>();
+
+        //Define vertices
+        Vector3 topLeftFront = new Vector3(1, 1, 0.5f);
+        Vector3 topLeftBack = new Vector3(1, 1, -0.5f);
+        Vector3 bottomLeftFront = new Vector3(1, 0, 0.5f);
+        Vector3 bottomLeftBack = new Vector3(1, 0, -0.5f);
+        Vector3 bottomRightFront = new Vector3(0, 0, 0.5f);
+        Vector3 bottomRightBack = new Vector3(0, 0, -0.5f);
+
+        //Assign vertices
+        Vector3[] verts = new Vector3[6];
+        verts[0] = topLeftFront;
+        verts[1] = topLeftBack;
+        verts[2] = bottomLeftFront;
+        verts[3] = bottomLeftBack;
+        verts[4] = bottomRightFront;
+        verts[5] = bottomRightBack;
+
+        //Create UVs
+        Vector2[] uvs = new Vector2[newMesh.vertices.Length];
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(newMesh.vertices[i].x, newMesh.vertices[i].z);
+        }
+
+        //Create triangles
+
+        // x+ face 1, 0, 3, 3, 0, 2
+        // y- face 3, 2, 5, 5, 2, 4
+        // x- face 0, 1, 4, 4, 1, 5
+        // z+ face 0, 4, 2
+        // z- face 1, 3, 5
+
+        int[] tris = new int[18];
+
+        // z+ face
+        tris[0] = 4;
+        tris[1] = 2;
+        tris[2] = 0;
+
+        // x- face
+        tris[3] = 0;
+        tris[4] = 1;
+        tris[5] = 4;
+        tris[6] = 4;
+        tris[7] = 1;
+        tris[8] = 5;
+
+        // z- face
+        tris[9] = 1;
+        tris[10] = 3;
+        tris[11] = 5;
+
+        // x+ face
+        tris[12] = 1;
+        tris[13] = 0;
+        tris[14] = 3;
+        tris[15] = 3;
+        tris[16] = 0;
+        tris[17] = 2;
+
+        // --
+
+        //Assign vertices
+        newMesh.vertices = verts;
+
+        //Assign uvs
+        newMesh.uv = uvs;
+
+        //Assign triangles
+        newMesh.triangles = tris;
+
+
+        //Recalculate bounds before normals
+        newMesh.RecalculateBounds();
+
+        //Recalculate normals
+        newMesh.RecalculateNormals();
+
+        //Optimize
+        ;
+
+        //Name
+        newMesh.name = "MyMesh";
+
+        //Assign mesh to filter
+        GetComponent<MeshFilter>().mesh = newMesh;
+
+        /**
         mVerts = new Vector3[4];
         mUV = new Vector2[4];
         mTris = new int[2 * 3];
 
         Mesh mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+
+        //GetComponent<MeshFilter>().mesh = mesh;
 
         mVerts[0] = new Vector3(-1.0f, 0.0f, 1.0f);
         mVerts[1] = new Vector3(1.0f, 0.0f, 1.0f);
@@ -39,6 +140,8 @@ public class MeshGenerator : MonoBehaviour {
 
         mesh.RecalculateBounds();
         mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;**/
 
     }
 
--- Assets/Scripts/Terrain.cs
+++ Assets/Scripts/Terrain.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class Terrain : MonoBehaviour {
     public int mDivisions;  // FIXME: number of faces
-    public float mSize;     // size of the terrain (e.g 10 x 10）
+    public float mSize;
+    public float length;     // size of the terrain (e.g 10 x 10）
     public float mHeight;   // max height of the terrain
 
     Vector3[] mVerts;
@@ -55,6 +56,35 @@ public class Terrain : MonoBehaviour {
         }
 
 
+        mVerts[0].y = Random.Range(-mHeight, mHeight);              // top left corner
+        mVerts[mDivisions].y = Random.Range(-mHeight, mHeight);     // top right corner
+        mVerts[mVerts.Length - 1].y = Random.Range(-mHeight, mHeight);                // bottom right corner
+        mVerts[mVerts.Length - mDivisions - 1].y = Random.Range(-mHeight, mHeight);
+
+        int iterations = (int)Mathf.Log(mDivisions, 2);
+        int numSquares = 1;
+        int squareSize = mDivisions;
+        for (int i = 0; i < iterations; i++)
+        {
+            int row = 0;
+            for (int j = 0; j < numSquares; j++)
+            {
+                int col = 0;
+                for (int k = 0; k < numSquares; k++)
+                {
+                    DiamondSquare(row, col, squareSize, mHeight);
+                    col += squareSize;
+                }
+                row += squareSize;
+            }
+            numSquares *= 2;
+            squareSize /= 2;
+            mHeight *= 0.5f;
+
+        }
+
+
+
         mesh.vertices = mVerts;
         mesh.uv = uvs;
         mesh.triangles = tris;
@@ -67,4 +97,23 @@ public class Terrain : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    // run diamondSquare on each square
+    void DiamondSquare(int row, int col, int size, float offset)
+    {
+        int halfSize = (int)(size * 0.5f);
+        int topLeft = row * (mDivisions + 1) + col;
+        int bottomLeft = (row + size) * (mDivisions + 1) + col;
+
+        // middle point of the square
+        // diamond part
+        int mid = (int)(row + halfSize) * (mDivisions + 1) + (int)(col + halfSize);
+        mVerts[mid].y = (mVerts[topLeft].y + mVerts[topLeft + size].y + mVerts[bottomLeft].y + mVerts[bottomLeft + size].y) * 0.25f + Random.Range(-offset, offset);
+
+        // square part
+        mVerts[topLeft + halfSize].y = (mVerts[topLeft].y + mVerts[topLeft + size].y + mVerts[mid].y) / 3 + Random.Range(-offset, offset);
+        mVerts[mid - halfSize].y = (mVerts[topLeft].y + mVerts[bottomLeft].y + mVerts[mid].y) / 3 + Random.Range(-offset, offset);
+        mVerts[mid + halfSize].y = (mVerts[topLeft + size].y + mVerts[bottomLeft + size].y + mVerts[mid].y) / 3 + Random.Range(-offset, offset);
+        mVerts[bottomLeft + halfSize].y = (mVerts[bottomLeft].y + mVerts[bottomLeft + size].y + mVerts[mid].y) / 3 + Random.Range(-offset, offset);
+    }
 }
