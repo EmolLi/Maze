86ba24b7a5334f093b218f96453c43b671500ac6
Emol Li <duan.li@mail.mcgill.ca>
2017-09-25 16:36:08 -0400

    maze entry

---
+++ Assets/Scripts/MazeEntrance.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeEntrance : MonoBehaviour {
+    int enterCnt;
+    private bool ableToIncrement;
+    public Material[] materials;
+    private Renderer rend;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && ableToIncrement)
+        {   
+            this.enterCnt++;
+            this.ableToIncrement = false;
+            if (enterCnt < 2)
+            {
+                rend.sharedMaterial = materials[enterCnt];
+            }
+            if (enterCnt == 2)
+            {
+                other.gameObject.layer = 8;
+                Destroy(gameObject);
+                Debug.Log(other.gameObject.layer);
+            }
+
+        } 
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" && !ableToIncrement)
+        {
+            ableToIncrement = true;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        enterCnt = -1; 
+        ableToIncrement = true;
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TerrainDepthMask.shader
@@ -0,0 +1,11 @@
+﻿Shader "TerrainDepthMask" {
+  SubShader {
+    Tags {"Queue" = "Geometry+10" } // earlier = hides stuff later in queue
+    Lighting Off
+    ZTest LEqual
+    ZWrite On
+    ColorMask 0
+    Pass {}
+  }
+}
+
